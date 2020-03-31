e0606f77f0e7c9e9a9c4ed2fe2d2cfaa2c678fa7
santa-sukitoku <santanaruse@gmail.com>
2020-03-31 20:01:16 +0900

    feat: scriptの基礎を理解

---
+++ Assets/Test.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, World!");
+
+        int age;
+        age = 30;
+        Debug.Log(age);
+
+        float height1 = 160.5f;
+        float height2;
+        height2 = height1;
+        Debug.Log(height2);
+
+        string name;
+        name = "kitamura";
+        Debug.Log(name);
+
+        int answer;
+        answer = 1 + 2;
+        Debug.Log(answer);
+        answer = 3 - 4;
+        Debug.Log(answer);
+        answer = 6 * 5;
+        Debug.Log(answer);
+        answer = 8 / 4;
+        Debug.Log(answer);
+
+        int n1 = 8;
+        int n2 = 9;
+        answer = n1 + n2;
+        Debug.Log(answer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
