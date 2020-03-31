4b704798eb806df37299c4edd469b7aec6464288
santa-sukitoku <santanaruse@gmail.com>
2020-03-31 22:33:56 +0900

    feat: classを理解

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,6 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Player {
+    private int hp = 100;
+    private int power = 50;
+
+    public void Attack() {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+    public void Damage(int damage) {
+        this.hp -= damage;
+        Debug.Log(damage + "のダメージを受けた");
+    }
+}
+
 public class Test : MonoBehaviour
 {
     void callName(string name) {
@@ -109,6 +122,23 @@ public class Test : MonoBehaviour
         int secondAnswer;
         secondAnswer = add(3, 4);
         Debug.Log(answer);
+
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
+
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
+
+        Vector2 startPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos - startPos;
+        Debug.Log(dir);
+
+        float len = dir.magnitude;
+        Debug.Log(len);
     }
 
     // Update is called once per frame
