e6f3d6c9d2d0a2be8bba54231e308b728a78d814
santa-sukitoku <santanaruse@gmail.com>
2020-03-31 21:26:05 +0900

    feat: 配列を理解

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -72,6 +72,28 @@ public class Test : MonoBehaviour
             sum += 1;
         }
         Debug.Log(sum);
+
+        int[] array = new int[5];
+        array[0] = 2;
+        array[1] = 10;
+        array[2] = 5;
+        array[3] = 15;
+        array[4] = 3;
+        for (int i = 0; i < 5; i++) {
+            Debug.Log(array[i]);
+        }
+
+        int[] point = { 83, 99, 52, 93, 15 };
+        int secondSum = 0;
+        for (int i = 0; i < point.Length; i++) {
+            secondSum += point[i];
+            if (point[i] >= 90) {
+                Debug.Log(point[i]);
+            }
+        }
+        //int average = secondSum / point.Length;
+        float average = 1.0f * secondSum / point.Length;
+        Debug.Log(average);
     }
 
     // Update is called once per frame
